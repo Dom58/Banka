5742f3fe2ed5a145d5083c88f8f06b64e7a2a92c
Dom58 <you@example.com>
2019-04-10 10:16:37 +0000

    add an banka api servre with name api and give a port to be listened on

--- server/app.js
+++ server/app.js
@@ -1,18 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-const path = require('path');
+
+import users from './routes/users';
+// import accountRouter from './routes/accountRouter';
+
+import path from 'path';
 
 var app = express();
+// const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// const port = 3000;
+app.use(users);
+// app.use(accountRouter);
+
 
 app.get('/', (req,res)=>{
-	res.send('Welcome to Banka application');
+	res.send('<h2>Welcome to Banka application ...</h2>');
 });
 
-const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
 	console.log(`Server is running on (http://127.0.0.1:${port}) `);
