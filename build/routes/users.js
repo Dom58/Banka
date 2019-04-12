900963ea16b2a9fc2adad1c04fdcbf108c5e44c2
Dom58 <you@example.com>
2019-04-12 21:28:07 +0000

    changes of build after compiling

--- build/routes/users.js
+++ build/routes/users.js
@@ -7,7 +7,7 @@ exports["default"] = void 0;
 
 var _express = _interopRequireDefault(require("express"));
 
-var _userController = _interopRequireDefault(require("../controllers/userController"));
+var _UserController = _interopRequireDefault(require("../controllers/UserController"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
@@ -15,7 +15,7 @@ var path = require('path');
 
 var route = _express["default"].Router();
 
-route.post('/api/v1/auth/signup', _userController["default"].signUp);
-route.post('/api/v1/auth/signin', _userController["default"].signIn);
+route.post('/api/v1/auth/signup', _UserController["default"].signUp);
+route.post('/api/v1/auth/signin', _UserController["default"].signIn);
 var _default = route;
 exports["default"] = _default;
